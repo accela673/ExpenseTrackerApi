027656302f8fe1108e28db61f4bd0da169d9019c
Сардар Касмалиев <sardarkasm81@gmail.com>
2026-09-02 17:45:00 +0600

    initial project setup

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using ExpenseTrackerApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackerApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>()
+            .HasIndex(c => c.Name)
+            .IsUnique();
+
+        modelBuilder.Entity<Category>()
+            .HasMany(c => c.Transactions)
+            .WithOne(t => t.Category)
+            .HasForeignKey(t => t.CategoryId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
